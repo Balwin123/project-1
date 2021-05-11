c64b1860a5b87fc826d59238730dcf6123b21a1b
Balwin123 <balwinmk@gmail.com>
2021-05-11 10:35:23 -0700

    added about section

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
+            $('.navbar').addClass("sticky"); 
         }else{
             $('.navbar').removeClass("sticky");
         }
     });
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass('active');
+        $('.navbar .menu').toggleClass("active")
+        $('..menu-btn i').toggleClass("active")
     });
 });
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 20){
+//             $('.navbar').addClass("sticky");
+//         }else{
+//             $('.navbar').removeClass("sticky");
+//         }
+//     });
+//     // toggle menu/navbar script
+//     $('.menu-btn').click(function(){
+//         $('.navbar .menu').toggleClass('active');
+//     });
+// });
